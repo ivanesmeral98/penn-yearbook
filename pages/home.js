f30d602f206f9fdfc29601511c814546106d6b73
Eva Killenberg <>
2021-04-10 13:13:47 -0400

    Home page alpha filtering for students

--- pages/home.js
+++ pages/home.js
@@ -1,39 +1,52 @@
 import { withIronSession } from 'next-iron-session'
 import { useState } from 'react'
 import Student from '../components/student'
+import { alphaFilters } from '../helpers/constants'
+
+const sections = ['students', 'groups', 'notes']
 
 export default function Home({ user, users }) {
     const [section, setSection] = useState('students')
+    const [filter, setFilter] = useState()
 
     return (
         <div className="home">
             <div className="header-container">
                 <p className="header">Hey, {user.firstName}</p>
                 <div className="header-links">
-                    <p
-                        className={section === 'students' && 'active'}
-                        onClick={() => setSection('students')}
-                    >
-                        Students
-                    </p>
-                    <p
-                        className={section === 'groups' && 'active'}
-                        onClick={() => setSection('groups')}
-                    >
-                        Groups
-                    </p>
-                    <p
-                        className={section === 'notes' && 'active'}
-                        onClick={() => setSection('notes')}
-                    >
-                        Notes
-                    </p>
+                    {sections.map((s) => (
+                        <p
+                            className={section === s && 'active'}
+                            onClick={() => setSection(s)}
+                            key={s}
+                        >
+                            {s}
+                        </p>
+                    ))}
                 </div>
             </div>
-            <div className="user-grid">
-                {users.map((u) => (
-                    <Student key={u.email} user={user} />
-                ))}
+            <div className="is-flex">
+                <div className="user-grid">
+                    {users.map((u) =>
+                        filter &&
+                        u.firstName.charAt(0).toLowerCase() <
+                            filter.toLowerCase() ? null : (
+                            <Student key={u.email} user={user} />
+                        ),
+                    )}
+                </div>
+                <div className="filters">
+                    {section === 'students' &&
+                        alphaFilters.map((a) => (
+                            <p
+                                className={filter === a && 'active'}
+                                onClick={() => setFilter(a)}
+                                key={a}
+                            >
+                                {a}
+                            </p>
+                        ))}
+                </div>
             </div>
         </div>
     )
