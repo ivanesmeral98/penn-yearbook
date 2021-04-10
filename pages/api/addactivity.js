a30f2a00092e1be2e719a7d3dc1298c22e832ea8
Ivan Esmeral <ivanes@seas.upenn.edu>
2021-04-10 19:36:59 -0400

    implement add activity

---
+++ pages/api/addactivity.js
@@ -0,0 +1,22 @@
+import db from '../../utils/firestore-backend'
+
+export default function addactivity(req, res) {
+    console.log('entered')
+    if (req.method === 'POST') {
+        // Process a POST request
+        const { name } = req.body
+        db.collection('activities')
+            .doc(name)
+            .set({ name })
+            .then(() => {
+                res.status(200).send({ name })
+            })
+            .catch((err) => {
+                res.status(403).send({
+                    err: `Error putting user in DB${err}`,
+                })
+            })
+    } else {
+        // Handle any other HTTP method
+    }
+}
