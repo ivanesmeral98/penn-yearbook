46e80e94da05c6a4bce2433caef5ac35bc4560ec
Ivan Esmeral <ivanes@seas.upenn.edu>
2021-04-10 14:01:57 -0400

    updating env variables

--- utils/firestore-backend.js
+++ utils/firestore-backend.js
@@ -2,16 +2,16 @@ import admin from 'firebase-admin'
 
 // Credentials accessed through environment
 const serviceAccount = {
-    type: process.env.TYPE,
-    project_id: process.env.PROJECT_ID,
-    private_key_id: process.env.PRIVATE_KEY_ID,
-    private_key: process.env.PRIVATE_KEY,
-    client_email: process.env.CLIENT_EMAIL,
-    client_id: process.env.CLIENT_ID,
-    auth_uri: process.env.AUTH_URI,
-    token_uri: process.env.TOKEN_URI,
-    auth_provider_x509_cert_url: process.env.PROVIDER_CERT_URL,
-    client_x509_cert_url: process.env.CLIENT_CERT_URL,
+    type: process.env.NEXT_PUBLIC_TYPE,
+    project_id: process.env.NEXT_PUBLIC_PROJECT_ID,
+    private_key_id: process.env.NEXT_PUBLIC_PRIVATE_KEY_ID,
+    private_key: process.env.NEXT_PUBLIC_PRIVATE_KEY,
+    client_email: process.env.NEXT_PUBLIC_CLIENT_EMAIL,
+    client_id: process.env.NEXT_PUBLIC_CLIENT_ID,
+    auth_uri: process.env.NEXT_PUBLIC_AUTH_URI,
+    token_uri: process.env.NEXT_PUBLIC_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.NEXT_PUBLIC_PROVIDER_CERT_URL,
+    client_x509_cert_url: process.env.NEXT_PUBLIC_CLIENT_CERT_URL,
 }
 
 if (!admin.apps.length) {
