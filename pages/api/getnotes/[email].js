11d43b8b0e9494af9e0a3220096de60b6ecb1885
Ivan Esmeral <ivanes@seas.upenn.edu>
2021-04-10 16:53:14 -0400

    get notes functionality

---
+++ pages/api/getnotes/[email].js
@@ -0,0 +1,25 @@
+import db from '../../../utils/firestore-backend'
+
+export default function getNotesPerUser(req, res) {
+    if (req.method === 'GET') {
+        const vals = []
+        const { email } = req.query
+        const notes = db.collection('notes')
+        notes
+            .where('toEmail', '==', email)
+            .get()
+            .then((snapshot) => {
+                if (snapshot.empty) {
+                    res.status(404).send({ Message: 'No notes found!' })
+                }
+                snapshot.forEach((doc) => {
+                    vals.push({ ...doc.data(), id: doc.id })
+                })
+                res.send({ notes: vals })
+            })
+            .catch((err) => {
+                console.log(err)
+                res.status(500).send({ ERR: err })
+            })
+    }
+}
