c369bf52e9a2d62c2dbc6b4ad830879d0f227c99
Ivan Esmeral <ivanes@seas.upenn.edu>
2021-04-11 02:41:39 -0400

    add to group implemented

---
+++ pages/api/addtogroup.js
@@ -0,0 +1,27 @@
+import admin from 'firebase-admin'
+import db from '../../utils/firestore-backend'
+
+export default async function addToGroup(req, res) {
+    if (req.method === 'POST') {
+        // Process a POST request
+        const { groupName, email } = req.body
+
+        const groupOfInterest = db.collection('groups').doc(groupName)
+        await groupOfInterest
+            .update({
+                members: admin.firestore.FieldValue.arrayUnion(
+                    db.doc(`users/${email}`),
+                ),
+            })
+            .then(() => {
+                res.status(200).send({
+                    Message: 'Successfully added to group',
+                })
+            })
+            .catch((err) => {
+                res.status(500).send(err)
+            })
+    } else {
+        res.status(404).send()
+    }
+}
